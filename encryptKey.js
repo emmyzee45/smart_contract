4922c407d777c89a157624d5c43a7a356b4aa771
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2023-03-06 23:21:57 +0100

    first commit

--- encryptKey.js
+++ encryptKey.js
@@ -4,7 +4,24 @@ require("dotenv").config();
 
 async function main() {
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
-  const encryptedJsonKey = await ethers.encryptKeystoreJson(
-    process.env.PRIVATE_KEY_PASSWORD
-  );
+
+  //   const encryptedJsonKey = await wallet.encrypt(
+  //     process.env.PRIVATE_KEY_PASSWORD,
+  //     process.env.PRIVATE_KEY
+  //   );
+  //   console.log(encryptedJsonKey);
+  const encryptedWallet = await wallet
+    .encrypt(process.env.PRIVATE_KEY_PASSWORD)
+    .then((encryptedWallet) => {
+      return encryptedWallet;
+    });
+  console.log(encryptedWallet);
+  fs.writeFileSync("./.encryptedKey.json", encryptedWallet);
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
